27aaf99ce22b852896c809555d84fc15ab25491b
Bryan <bnaufalathailla12@gmail.com>
2026-05-29 21:17:36 +0700

    aseli

--- src/main/java/com/kasirqu/services/update/UpdateInventoryService.java
+++ src/main/java/com/kasirqu/services/update/UpdateInventoryService.java
@@ -1,9 +1,33 @@
 package com.kasirqu.services.update;
 
 import com.kasirqu.models.Barang;
+import com.kasirqu.repositories.update.UpdateInventoryRepository;
 
 public class UpdateInventoryService {
-    public boolean updateProduct(Barang barang) {
-        throw new UnsupportedOperationException("Not implemented yet");
+
+    private final UpdateInventoryRepository repository =
+            new UpdateInventoryRepository();
+
+    public boolean updateBarang(Barang barang) {
+
+        if (barang == null) {
+            return false;
+        }
+
+        if (barang.getNamaBarang() == null ||
+                barang.getNamaBarang().isBlank()) {
+
+            return false;
+        }
+
+        if (barang.getHarga() == null) {
+            return false;
+        }
+
+        if (barang.getStok() < 0) {
+            return false;
+        }
+
+        return repository.updateBarang(barang);
     }
 }
